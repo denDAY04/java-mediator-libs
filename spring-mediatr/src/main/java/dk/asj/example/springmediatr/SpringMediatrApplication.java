28db40e60ba50ace2bc5ec536e6092a262d60f79
Andreas Stensig <stensig04@gmail.com>
2021-10-11 16:47:33 +0200

    spring-mediatr init

---
+++ spring-mediatr/src/main/java/dk/asj/example/springmediatr/SpringMediatrApplication.java
@@ -0,0 +1,13 @@
+package dk.asj.example.springmediatr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMediatrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMediatrApplication.class, args);
+	}
+
+}
---
+++ spring-mediatr/src/test/java/dk/asj/example/springmediatr/SpringMediatrApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.asj.example.springmediatr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMediatrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
